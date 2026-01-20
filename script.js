b5e0968cb989d79405fb9f8e3417c992b7e75a58
jyothsna-rajanala <jyoshnapavani780@gmail.com>
2026-01-20 19:35:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function showWish(option) {
 
     document.getElementById("wishBox").innerText = wish;
 }
+function confettiEffect() {
+  for (let i = 0; i < 30; i++) {
+    const confetti = document.createElement("div");
+    confetti.classList.add("confetti");
+    confetti.style.left = Math.random() * 100 + "vw";
+    confetti.style.animationDuration = (Math.random() * 3 + 2) + "s";
+    document.body.appendChild(confetti);
+
+    setTimeout(() => {
+      confetti.remove();
+    }, 5000);
+  }
+}
