8eb701ae21ad3f747c64e9ece1cdd5cffea23f54
jyothsna-rajanala <jyoshnapavani780@gmail.com>
2026-03-08 15:59:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 function showWish(option) {
+
   let wish = "";
 
   if (option === 1) {
     wish = "Today and everyday wishing only the best for you💞🫂";
-  } 
+  }
   else if (option === 2) {
     wish = "Wishing you happiness , success and lots of beautiful moments this year🩷🤗";
-  } 
+  }
   else if (option === 3) {
     wish = "I was going to get you a perfect gift but then i realized meeting me already a best gift😁💓";
   }
 
   const box = document.getElementById("wishBox");
+
   box.classList.remove("pop");
   void box.offsetWidth;
   box.classList.add("pop");
@@ -19,16 +21,21 @@ function showWish(option) {
   box.innerText = wish;
 }
 
+
+/* Falling hearts effect */
+
 function confettiEffect() {
 
-  const hearts = ["💗","💖","💞","💕","💙","🩵","💓"];
+  const hearts = ["🩷","🩵"];
 
-  for (let i = 0; i < 30; i++) {
+  for (let i = 0; i < 25; i++) {
 
     const confetti = document.createElement("div");
+
     confetti.classList.add("confetti");
 
     confetti.style.left = Math.random() * 100 + "vw";
+
     confetti.style.animationDuration = (Math.random() * 3 + 2) + "s";
 
     confetti.innerHTML = hearts[Math.floor(Math.random() * hearts.length)];
@@ -39,7 +46,40 @@ function confettiEffect() {
       confetti.remove();
     }, 5000);
   }
+
 }
+
+
+/* Background floating hearts */
+
+function backgroundHearts() {
+
+  const hearts = ["🩷","🩵"];
+
+  setInterval(() => {
+
+    const heart = document.createElement("div");
+
+    heart.classList.add("bg-heart");
+
+    heart.innerHTML = hearts[Math.floor(Math.random()*hearts.length)];
+
+    heart.style.left = Math.random()*100 + "vw";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+      heart.remove();
+    },12000);
+
+  },800);
+
+}
+
+backgroundHearts();
+
+
+/* Celebrate button */
 
 function celebrate() {
 
@@ -48,10 +88,14 @@ function celebrate() {
   confettiEffect();
 
   const box = document.getElementById("wishBox");
+
   box.classList.remove("pop");
+
   void box.offsetWidth;
+
   box.classList.add("pop");
 
   box.innerText =
   "You are such a special person in my life whom i cherish dearly may your birthday today bring you a step closer to realizing every beautiful dream of yours 💗💙✨";
+
 }
